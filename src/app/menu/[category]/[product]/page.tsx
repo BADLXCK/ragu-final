b09e4336dff55d92c57800ada935e167fc5cac0f
badlxck <jmovie@yandex.ru>
2026-05-14 06:26:19 +0300

    fix

--- src/app/menu/[category]/[product]/page.tsx
+++ src/app/menu/[category]/[product]/page.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from 'next';
 import { getSeoByUri } from '@/api/queries/getSeoByUri';
-import { ProductPage } from "@/routes/product";
+import { ProductPage } from '@/routes/product';
 
 const BASE_URI = '/product/';
 
 type Props = {
-  params: Promise<{ category: string; product: string }>;
+	params: Promise<{ category: string; product: string }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { product } = await params;
-  return await getSeoByUri(`${BASE_URI}${product}/`);
+	const { product } = await params;
+	return await getSeoByUri(`${BASE_URI}${product}/`);
 }
 
 export const dynamicParams = true;
--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,16 +1,15 @@
 import { Metadata } from 'next';
 import { getSeoByUri } from '@/api/queries/getSeoByUri';
-import { CategoryPage } from "@/routes/category";
+import { CategoryPage } from '@/routes/category';
 
 const BASE_URI = '/product-category/';
 
-type Props = {
-  params: Promise<{ category: string }>;
-};
+export async function generateMetadata({
+	params,
+}: PageProps<'/menu/[category]'>): Promise<Metadata> {
+	const { category } = await params;
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { category } = await params;
-  return await getSeoByUri(`${BASE_URI}${category}/`);
+	return await getSeoByUri(`${BASE_URI}${category}/`);
 }
 
 export default CategoryPage;
